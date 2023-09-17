2fb5a6b7ff46933e01fd912b7fb25c8177313561
Soyka <petr.emelianov@icloud.com>
2023-09-17 18:16:00 +0300

    main syntax

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import string
 import time
 import os
 
+def generate_username():
+    pass
+
+def check_mail():
+    pass
+
+def delete_mail():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
